be3137d747a024b590606440669ec076be5eb6f6
Andreas Radke <mail@andreasradke.de>
2020-04-27 10:24:33 +0200

    update client.sh/bat and daemon.sh/bat

--- client.sh
+++ client.sh
@@ -4,4 +4,4 @@
 JAVA_HOME=/bin/java
 TAPAS_JAR=build/libs/TAPAS-all.jar
 
-$JAVA_HOME -cp $TAPAS_JAR de.dlr.ivf.tapas.runtime.server.SimulationControl
+$JAVA_HOME -cp $TAPAS_JAR de.dlr.ivf.tapas.runtime.client.SimulationControl
--- daemon.sh
+++ daemon.sh
@@ -4,5 +4,6 @@
 JAVA_HOME=/bin/java
 TAPAS_JAR=build/libs/TAPAS-all.jar
 SIM_DIR=data/Simulations
+JAVA_ARGS=-Xmx64G
 
-$JAVA_HOME -cp $TAPAS_JAR de.dlr.ivf.tapas.runtime.server.SimulationDaemon $SIM_DIR
+$JAVA_HOME $JAVA_ARGS -cp $TAPAS_JAR de.dlr.ivf.tapas.runtime.server.SimulationDaemon $SIM_DIR
