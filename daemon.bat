b8ebd6f77826ebba033b49a20e69b326a25f847b
Andreas Radke <andreas.radke@dlr.de>
2020-04-27 10:39:31 +0200

    change JAVA_ARGS in daemon.bat to smaller ram size

--- daemon.bat
+++ daemon.bat
@@ -1,7 +1,7 @@
 set MAIN=de.dlr.ivf.tapas.runtime.server.SimulationDaemon
 set JAR=build/libs/TAPAS-all.jar
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-set JAVA_ARGS=-Xmx64g
+set JAVA_ARGS=-Xmx8g
 set RUNTIME_DIR=%CD%\data\Simulations
 
 %JAVA_EXE% %JAVA_ARGS% -cp "%JAR%" %MAIN% %RUNTIME_DIR%
