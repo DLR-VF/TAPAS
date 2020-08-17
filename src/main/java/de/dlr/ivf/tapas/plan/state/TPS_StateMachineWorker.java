59bb4081ef35cbd92a7fe89e81598e7ccbd62cef
motaa <alain.schengen@gmail.com>
2020-08-17 08:47:35 +0200

    - added a failsafe to not finishing state machines for further debugging

--- src/main/java/de/dlr/ivf/tapas/plan/state/TPS_StateMachineWorker.java
+++ src/main/java/de/dlr/ivf/tapas/plan/state/TPS_StateMachineWorker.java
@@ -4,6 +4,8 @@ import de.dlr.ivf.tapas.plan.TPS_Plan;
 import de.dlr.ivf.tapas.plan.state.event.TPS_PlanEvent;
 import de.dlr.ivf.tapas.plan.state.event.TPS_PlanEventType;
 import de.dlr.ivf.tapas.plan.state.statemachine.TPS_PlanStateMachine;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
@@ -14,11 +16,13 @@ public class TPS_StateMachineWorker implements Runnable {
     private CyclicBarrier cb;
     private TPS_PlansExecutor executor;
     private boolean i_am_done = false;
+    private List<TPS_PlanStateMachine> unfinished_mashines = new ArrayList<>();
 
     public TPS_StateMachineWorker(List<TPS_PlanStateMachine<TPS_Plan>> state_machines, TPS_PlansExecutor executor){
         this.state_machines = state_machines;
         this.cb = executor.getCyclicBarrier();
         this.executor = executor;
+
     }
 
 
@@ -29,16 +33,20 @@ public class TPS_StateMachineWorker implements Runnable {
     @Override
     public void run() {
         while(this.executor.getNextSimulationEvent().getEventType() != TPS_PlanEventType.END_OF_SIMULATION){
+            int simcount = (int) executor.getNextSimulationEvent().getData();
             int active_state_machines = 0;
             TPS_PlanEvent event = this.executor.getNextSimulationEvent();
             if(!i_am_done) {
                 for (TPS_PlanStateMachine<TPS_Plan> state_machine : state_machines) {
                     if (!state_machine.hasFinished()) {
+                        if(simcount > 5000)
+                            unfinished_mashines.add(state_machine);
+
                         active_state_machines++;
                         state_machine.handleEvent(event);
                     }
                 }
-                i_am_done = active_state_machines == 0;
+                i_am_done = simcount > 5000 ? true : active_state_machines == 0;
 
                 //when we are done we need to tell the executor once
                 if(i_am_done)
