ade49603540b53202659b89c37dbab6ccedaed70
motaa <alain.schengen@gmail.com>
2021-11-20 14:31:09 +0100

    - added comments

--- src/main/java/de/dlr/ivf/tapas/execution/sequential/action/UpdateModeChoicePlanAttributesAction.java
+++ src/main/java/de/dlr/ivf/tapas/execution/sequential/action/UpdateModeChoicePlanAttributesAction.java
@@ -5,10 +5,17 @@ import de.dlr.ivf.tapas.plan.TPS_LocatedStay;
 import de.dlr.ivf.tapas.plan.TPS_Plan;
 import de.dlr.ivf.tapas.util.TPS_AttributeReader;
 
+/**
+ * Updates mode choice tree parameters
+ */
 public class UpdateModeChoicePlanAttributesAction implements TPS_PlanStateAction {
     private final TPS_Plan plan;
     private final TPS_LocatedStay next_located_stay;
 
+    /**
+     * @param plan the plan of a person
+     * @param next_located_stay the next located stay
+     */
     public UpdateModeChoicePlanAttributesAction(TPS_Plan plan, TPS_LocatedStay next_located_stay) {
 
         this.plan = plan;
@@ -21,6 +28,8 @@ public class UpdateModeChoicePlanAttributesAction implements TPS_PlanStateAction
         plan.getAttributes().put(TPS_AttributeReader.TPS_Attribute.CURRENT_TAZ_SETTLEMENT_CODE_TAPAS,
                 next_located_stay.getLocation().getTrafficAnalysisZone().getBbrType()
                         .getCode(TPS_SettlementSystem.TPS_SettlementSystemType.TAPAS));
+
+        //todo add distance category from actual location to dest location
 
     }
 }
