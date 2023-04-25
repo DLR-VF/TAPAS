8cb5ffed87c566afdc1d0c359edea03c296a1950
motaa <alain.schengen@gmail.com>
2023-04-25 21:43:40 +0200

    removed circular dependency with tapas-logger; added commons-math3

--- tapas-util/build.gradle.kts
+++ tapas-util/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    implementation(project(":tapas-logger"))
+    implementation("org.apache.commons:commons-math3:3.6.1")
 }
 
 tasks.test {
