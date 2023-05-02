7282cc1574574a3905687de1d9ed697b3b3fd416
motaa <alain.schengen@gmail.com>
2023-05-03 01:19:41 +0200

    extracted analyzer to own module

--- tapas-analyzer/build.gradle.kts
+++ tapas-analyzer/build.gradle.kts
@@ -12,6 +12,17 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.jfree:jfreechart:1.5.4")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("com.jgoodies:jgoodies-forms:1.9.0")
+    implementation("net.sourceforge.javacsv:javacsv:2.0")
+
+    implementation(project(":tapas-parameter"))
+    implementation(project(":tapas-logger"))
+    implementation(project(":io"))
+    implementation(project(":tapas-util"))
+    implementation(project(":tapas-tools"))
 }
 
 tasks.test {
--- tapas-analyzer/src/main/java/module-info.java
+++ tapas-analyzer/src/main/java/module-info.java
@@ -1,3 +1,23 @@
 module de.dlr.ivf.tapas.analyzer {
+
+    requires java.desktop;
+    requires java.sql;
+
+    requires org.jfree.jfreechart;
+    requires org.apache.commons.collections4;
+    requires jgoodies.forms;
+    requires javacsv;
+
+    requires de.dlr.ivf.tapas.parameter;
+    requires de.dlr.ivf.tapas.logger;
+
     exports de.dlr.ivf.tapas.analyzer.tum.constants;
+    exports de.dlr.ivf.tapas.analyzer.tum.regionanalyzer;
+    exports de.dlr.ivf.tapas.analyzer.tum.regionanalyzer.general;
+    exports de.dlr.ivf.tapas.analyzer.tum.databaseConnector;
+    exports de.dlr.ivf.tapas.analyzer.inputfileconverter;
+    exports de.dlr.ivf.tapas.analyzer.tum;
+    requires de.dlr.ivf.api.io;
+    requires de.dlr.ivf.tapas.util;
+    requires de.dlr.ivf.tapas.tools;
 }
