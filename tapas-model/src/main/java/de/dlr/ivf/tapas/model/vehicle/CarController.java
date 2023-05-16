143f0fa902342c6fe09028985d381a4786232fb5
motaa <alain.schengen@gmail.com>
2023-05-16 21:11:52 +0200

    introduced CarControllers that manage single cars

---
+++ tapas-model/src/main/java/de/dlr/ivf/tapas/model/vehicle/CarController.java
@@ -0,0 +1,55 @@
+package de.dlr.ivf.tapas.model.vehicle;
+
+import de.dlr.ivf.tapas.model.Timeline;
+import lombok.Getter;
+
+@Getter
+public class CarController {
+
+    private final TPS_Car car;
+
+    private final Timeline timeline;
+
+    private final double maxRange;
+    private double remainingRange;
+    private boolean tollPaid;
+
+    public CarController(TPS_Car car){
+        this.car = car;
+        this.maxRange = car.fuelType().getRange();
+        this.remainingRange = maxRange;
+        this.timeline = new Timeline();
+    }
+
+    public boolean useCar(int startTime, int duration){
+        return timeline.add((int) (startTime + 0.5), (int) (startTime + duration + 0.5));
+    }
+
+    public boolean isCarAvailable(int startTime, int duration){
+        return this.timeline.clash(startTime, startTime + duration);
+    }
+
+    public void reduceRange(double range) {
+        this.remainingRange -= range;
+    }
+
+    public double remainingCarRange(){
+        return remainingRange;
+    }
+
+    public boolean isRestricted(){
+        return car.isRestricted();
+    }
+
+    public void payToll(){
+        this.tollPaid = true;
+    }
+
+    public void refuel(){
+        this.remainingRange = maxRange;
+    }
+
+    public int automationLevel(){
+        return car.getAutomationLevel();
+    }
+}
