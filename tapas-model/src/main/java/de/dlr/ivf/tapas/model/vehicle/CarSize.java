9a5dd0826114ee41548701cb8f91d695faf94d2a
motaa <alain.schengen@gmail.com>
2023-05-16 21:13:41 +0200

    extracted CarSize from TPS_Car

---
+++ tapas-model/src/main/java/de/dlr/ivf/tapas/model/vehicle/CarSize.java
@@ -0,0 +1,10 @@
+package de.dlr.ivf.tapas.model.vehicle;
+
+/**
+ * This enum represents all known car sizes in TAPAS
+ */
+
+
+public enum CarSize {
+    TRANSPORTER, SMALL, MEDIUM, LARGE, TRUCK75, TRUCK12, HEAVYDUTY, TRAILER, BUS, COACH
+}
