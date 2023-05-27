aa5ad70d2bb805c02807e4f3c65ea14b52c99fee
motaa <alain.schengen@gmail.com>
2023-05-27 12:40:01 +0200

    ResultSetConverter now also takes enums into account

--- io/src/main/java/de/dlr/ivf/api/io/implementation/ResultSetConverter.java
+++ io/src/main/java/de/dlr/ivf/api/io/implementation/ResultSetConverter.java
@@ -5,6 +5,8 @@ import de.dlr.ivf.api.io.SqlArrayUtils;
 import lombok.NonNull;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.Array;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -58,13 +60,34 @@ public class ResultSetConverter<T> extends ColumnMappingConverter<ResultSet, T> 
                         }
                     }
 
-                } else {
-                    field.set(convertedObject, valueToSet);
+                } else { //todo extract this to super class
+                    if (field.getType().isEnum()) {
+                        if(valueToSet instanceof String stringValue){ //try to map the enum from String
+                            Method valueOf = field.getType().getMethod("valueOf", String.class);
+                            Object enumConstant = valueOf.invoke(null, stringValue);
+                            field.set(convertedObject, enumConstant);
+                        }else{ //try mapping it to an int
+                            if(valueToSet instanceof Integer intValue) {
+                                Method ordinal = field.getType().getMethod("ordinal", Integer.class);
+                                Object enumConstant = ordinal.invoke(null, intValue);
+                                field.set(convertedObject, enumConstant);
+                            }
+                        }
+                    } else {
+                        field.set(convertedObject, valueToSet);
+                    }
                 }
             }catch (SQLException e){
                 e.printStackTrace();
             } catch (IllegalAccessException e) {
+                e.printStackTrace();
                 throw new RuntimeException("Can't access field: "+field.getName() + " in class: "+ className, e);
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Can't access method.", e);
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Unable to invoke method.", e);
             }
         }
         return convertedObject;
