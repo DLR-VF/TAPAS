12e5f8705a854a0c4fc7566b7cf02aa0a815ef1c
motaa <alain.schengen@gmail.com>
2023-06-14 07:59:32 +0200

    lots of cleanup, refactoring and new features

--- io/src/main/java/de/dlr/ivf/api/io/reader/implementation/BaseJdbcConnectionProvider.java
+++
@@ -1,6 +0,0 @@
-package de.dlr.ivf.api.io.reader.implementation;
-
-import de.dlr.ivf.api.io.JdbcConnectionProvider;
-
-public class BaseJdbcConnectionProvider implements JdbcConnectionProvider {
-}
