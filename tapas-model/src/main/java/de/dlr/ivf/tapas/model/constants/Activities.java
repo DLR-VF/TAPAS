954b2cfdebf13de623eb9a73b49d3c7f80ab9cd8
motaa <alain.schengen@gmail.com>
2024-06-29 15:26:15 +0200

    Add Activity class and update Activities class
    A new Activity class has been introduced, and the Activities class has been updated to accommodate this addition. Specifically, Activities class now has a constructor and it also includes a method to retrieve an activity by its Zbe code.

--- tapas-model/src/main/java/de/dlr/ivf/tapas/model/constants/Activities.java
+++ tapas-model/src/main/java/de/dlr/ivf/tapas/model/constants/Activities.java
@@ -1,18 +1,30 @@
 package de.dlr.ivf.tapas.model.constants;
 
 
-import lombok.AllArgsConstructor;
+import de.dlr.ivf.tapas.model.scheme.Activity;
 
 import java.util.Collection;
 
-@AllArgsConstructor
 public class Activities {
 
-    private final Collection<TPS_ActivityConstant> activities;
+    private final Collection<TPS_ActivityConstant> activityConstants;
+    private final Collection<Activity> activities;
+
+    public Activities(Collection<TPS_ActivityConstant> activityConstants, Collection<Activity> activities) {
+        this.activityConstants = activityConstants;
+        this.activities = activities;
+    }
 
     public TPS_ActivityConstant getActivity(TPS_ActivityConstant.TPS_ActivityCodeType type, int code){
-        return activities.stream()
+        return activityConstants.stream()
                 .filter(a -> a.getCode(type) == code)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("activity not found"));
+    }
+
+    public Activity getActivityByZbeCode(int code) {
+        return activities.stream()
+                .filter(a -> a.codeZbe() == code)
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("activity not found"));
     }
---
+++ tapas-model/src/main/java/de/dlr/ivf/tapas/model/scheme/Activity.java
@@ -0,0 +1,9 @@
+package de.dlr.ivf.tapas.model.scheme;
+
+public record Activity(
+        int codeZbe,
+        int codeTapas,
+        int codeVOT,
+        int codeMCT,
+        int priority) {
+}
